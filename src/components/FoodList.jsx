c9f6b09fc171040a67234ca3cb5339a190d0d235
Antony Omondi <tonymboya13@gmail.com>
2025-05-26 19:07:56 +0300

    Added fovorite toggle functionality

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -5,10 +5,15 @@ export default function FoodList({foods}){
 
   const [deletedIds, setDeletedIDs] = useState([]) // Tracks deleted foods
   const [favorites, setFavorites] = useState([])
+  const [showFavorites, setShowFavorites] = useState(false)
 
   function onDeleteFood(fdcId){
     setDeletedIDs(prev => [...prev, fdcId]) // Deleted foods are added to the deletedIds array
     setFavorites(prev => prev.filter(id => id !== fdcId)) // The deleted Id is removed in the favorites too
+  }
+
+  function toggleDisplay(){
+    setShowFavorites(!showFavorites)
   }
 
   // Checks if food is in the favorites array and removes it if so, else adds it.
@@ -17,22 +22,29 @@ export default function FoodList({foods}){
    }
 
     return(
-
-    <div className = "food-list">
+    <>
+      <br />
+      <button className='toggleFavorites' onClick={toggleDisplay}>
+          {showFavorites ? "Show All" : "Show Favorites"}
+      </button>
+    
+      <div className = "food-list">
       
-        {foods.filter(food => !deletedIds.includes(food.fdcId)).map(food=>(
-        <Food 
-        key = {food.fdcId} 
-        food = {food} 
-        isFavorite={favorites.includes(food.fdcId)}
-        onToggleFavorite={onToggleFavorite}
-        onDeleteFood={onDeleteFood}
-        />
-      ))}
+          {foods
+            .filter(food => !deletedIds.includes(food.fdcId))
+            .filter(food => !showFavorites || favorites.includes(food.fdcId))
+            .map(food=>(
+          <Food 
+          key = {food.fdcId} 
+          food = {food} 
+          isFavorite={favorites.includes(food.fdcId)}
+          onToggleFavorite={onToggleFavorite}
+          onDeleteFood={onDeleteFood}
+          />
+        ))}
 
-        
-    </div>
-
+      </div>
+    </>
 
     )
 }
