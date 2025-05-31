534e85cd8bd682ce98980cde5e05225b3ed16e48
Gregory Kipchumba <gregory.k182@gmail.com>
2025-05-31 19:36:34 +0300

    added MyProfile component

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import  { useAuth} from "./AuthContext";
+import { useNavigate } from 'react-router-dom';
 
 function MyProfile(){
-    
+    const { user } = useAuth();// Get current logged-in user from context
+    const navigate = useNavigate();
+
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+
+    if (!user || !storedUser) {
+        //Redirect to login if user is not logged in
+        navigate('/LoginForm');
+        return null; // Prevent rendering if not authenticated
+    }
 
     return(
-        <h1>This is My Profile page</h1>
+        <div>
+            <h2>My Profile</h2>
+            <p><strong>Username:</strong>{storedUser.username}</p>
+            <p><strong>Email:</strong>{storedUser.email}</p>
+            <p><strong>Password:</strong>{storedUser.password}</p>
+        </div>
 
     )
 }
