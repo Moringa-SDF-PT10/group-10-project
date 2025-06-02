ddf6c118610b3cef5342944b35210c01c569d702
Caroline Ininga <carolineininga@gmail.com>
2025-06-02 07:37:12 +0300

    added daily wellness log

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import PrivateRoute from './PrivateRoute';
 import MyProfile from './MyProfile';
 import MyFavorites from './MyFavorites';
 import MyDashboard from './MyDashboard';
-import MyGoals from './MyGoals';
+import DailyWellnessLog from './DailyWellnessLog';
 
 import AboutUs from './AboutUs';
 import  { useAuth} from "./AuthContext";
@@ -35,7 +35,7 @@ return(
         <Link to= {"/"}>Home</Link>
         <Link to= {"/MyFavorites"}>My Favorites</Link>
         <Link to= {"/FoodJournalForm"}>My Food Journal</Link>
-        <Link to= {"/MyGoals"}>My Goals</Link>
+        <Link to= {"/DailyWellnessLog"}>My Daily Wellness Log</Link>
         <Link to= {"/MyDashboard"}>My Dashboard</Link>
         <Link to= {"/LoginForm"}>Login Form</Link>
         <Link to= {"/SignUp"}>Sign Up</Link>
@@ -66,6 +66,7 @@ return(
             <Route path="/MyFavorites" element=  { <PrivateRoute><MyFavorites/></PrivateRoute>}/>      
             <Route path="/MyDashboard" element=  { <PrivateRoute><MyDashboard/></PrivateRoute>}/>   
             <Route path="/food/:fdcId" element=  { <PrivateRoute><FoodDetails /></PrivateRoute>}/> 
+            <Route path="/DailyWellnessLog" element=  { <PrivateRoute><DailyWellnessLog/></PrivateRoute>}/> 
           
                            
             
